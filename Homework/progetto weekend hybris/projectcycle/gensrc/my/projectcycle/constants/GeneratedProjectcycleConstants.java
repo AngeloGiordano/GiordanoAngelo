2310cc1bbfeb73cf576c9a0d60e295bb81d4b5c9
AngeloGiordano <giordano_angelo@outlook.it>
2017-10-29 21:09:49 +0100

    progetto weekend

---
+++ Homework/progetto weekend hybris/projectcycle/gensrc/my/projectcycle/constants/GeneratedProjectcycleConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 29-ott-2017 15.26.42                        ---
+ * ----------------------------------------------------------------
+ */
+package my.projectcycle.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedProjectcycleConstants
+{
+	public static final String EXTENSIONNAME = "projectcycle";
+	
+	protected GeneratedProjectcycleConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
